12411628eed1dd3bbaca39c2fd753ad7bfc3e261
Mladen Gligorijevic <mladen.og@gmail.com>
2013-01-08 20:19:52 +0100

    mladen3

--- src/modis/copsandrobber/CopsAndRobberApplication.java
+++ src/modis/copsandrobber/CopsAndRobberApplication.java
@@ -6,6 +6,7 @@ import android.content.Context;
 public class CopsAndRobberApplication extends Application{
 
 	private static CopsAndRobberApplication instance;
+	private int mladenGit;
 	
 	public CopsAndRobberApplication() {
 		instance = this;
